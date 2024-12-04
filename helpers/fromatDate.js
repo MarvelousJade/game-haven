31b4076d9bad6c27cabbf10f87f03d2ec661e685
shaoyu <fanshaoyu9@outlook.com>
2024-12-04 15:42:13 -0500

    Create helper fromatDate

---
+++ helpers/fromatDate.js
@@ -0,0 +1,7 @@
+
+module.exports = unction(dateObj){
+  let year = dateObj.getFullYear();
+  let month = (dateObj.getMonth() + 1).toString();
+  let day = dateObj.getDate().toString();
+  return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+}
